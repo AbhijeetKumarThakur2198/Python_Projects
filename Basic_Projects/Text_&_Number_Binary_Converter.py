3de88b1a195743ba14f8d8589085e61bd3d09f28
Abhijeet Kumar Thakur <156644805+AbhijeetKumarThakur2198@users.noreply.github.com>
2024-01-17 18:07:45 +0530

    Delete Basic_Projects/Text_&_Number_Binary_Converter.py

--- Basic_Projects/Text_&_Number_Binary_Converter.py
+++
@@ -1,8 +0,0 @@
-# MY PRACTICE PROJECT 1 USING PROCEDURAL PROGRAMMING
-def convert_to_binary(input):
-    get_binary = ' '.join(format(ord(character), 'b') for character in input)
-    return get_binary
-
-user_input = input("Enter an alphabet or number: ")
-binary_result = convert_to_binary(user_input)
-print(f"Binary representation: {binary_result}")
